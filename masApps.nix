be75e6875c08fd089ce4e3a145c624c017735763
Kellan Stevens <kellan@kellanstevens.com>
2024-10-13 20:09:30 +0200

    first commit

---
+++ masApps.nix
@@ -0,0 +1,9 @@
+{
+    "Xcode" = 497799835;
+    "Final Cut Pro" = 424389933;
+    "Ubiquiti WiFiman" = 1385561119;
+    "Mactracker" = 430255202;
+    "Apple Configurator" = 1037126344;
+    "Speedtest by Ookla" = 1153157709;
+    "TestFlight" = 899247664;
+}
