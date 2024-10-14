8d5b732a8ef71c0d9b5d49e1577f7c3e97a4364f
Kellan Stevens <kellan@kellanstevens.com>
2024-10-14 07:29:18 +0200

    chore(masApps.nix): added new appstore apps

--- masApps.nix
+++ masApps.nix
@@ -1,7 +1,6 @@
 {
     "Xcode" = 497799835;
     "Final Cut Pro" = 424389933;
-    "Ubiquiti WiFiman" = 1385561119;
     "Mactracker" = 430255202;
     "Apple Configurator" = 1037126344;
     "Speedtest by Ookla" = 1153157709;
