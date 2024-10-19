72fe6179000f67fecb26346dabff8f9cc2fa559f
Kellan Stevens <kellan@kellanstevens.com>
2024-10-19 23:11:07 +0200

    chore(home.nix): cleaup

--- home.nix
+++ home.nix
@@ -3,29 +3,16 @@
 { config, pkgs, ... }:
 
 let
-    vscodeConfig = import ./apps/vscode/vscode.nix { inherit config pkgs; };
+    vscodeConfig = import ./apps/vscode.nix { inherit config pkgs; };
 in
 {
-    home.stateVersion = "24.11"; # Please read the comment before changing.
-
-    home.file = {
-        # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-        # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-        # # symlink to the Nix store copy.
-        # ".screenrc".source = dotfiles/screenrc;
-
-        # # You can also set the file content immediately.
-        # ".gradle/gradle.properties".text = ''
-        #   org.gradle.console=verbose
-        #   org.gradle.daemon.idletimeout=3600000
-        # '';
-    };
-
-    home.sessionVariables = {
-        EDITOR = "nvim";
-    };
+    home.stateVersion = "24.11";
 
     programs.vscode = vscodeConfig.config.programs.vscode;
 
     programs.home-manager.enable = true;
+
+    home.sessionVariables = {
+        EDITOR = "nvim";
+    };
 }
