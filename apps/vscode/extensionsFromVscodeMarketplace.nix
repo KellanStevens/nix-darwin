a936316bc9c88dfdb5ea63e8671cf25d331bfc3a
Kellan Stevens <kellan@kellanstevens.com>
2024-10-20 19:55:57 +0200

    chore(apps/vscode/extensionsFromVscodeMarketplace.nix): finished exxtenstions

--- apps/vscode/extensionsFromVscodeMarketplace.nix
+++ apps/vscode/extensionsFromVscodeMarketplace.nix
@@ -1,4 +1,4 @@
-{ pkgs }: [
+{ pkgs, ... }: [
   # Looks
   {
       name = "moxer-icons";
@@ -35,12 +35,57 @@
       name = "vscode-css-peek";
       publisher = "pranaygp";
       version = "latest";
-      sha256 = "sha256-JbcygVMAsjF+jWZ7AlxkR+Q7skX1VYvgc0veSA1vsiU=";
+      sha256 = "sha256-GX6J9DfIW9CLarSCfWhJQ9vvfUxy8QU0kh3cfRUZIaE=";
   }
   {
       name = "es7-react-js-snippets";
       publisher = "dsznajder";
       version = "latest";
-      sha256 = "sha256-JbcygVMAsjF+jWZ7AlxkR+Q7skX1VYvgc0veSA1vsiU=";
+      sha256 = "sha256-QF950JhvVIathAygva3wwUOzBLjBm7HE3Sgcp7f20Pc=";
+  }
+  {
+      name = "vscode-nginx";
+      publisher = "william-voyek";
+      version = "latest";
+      sha256 = "sha256-mAmncewwAeagVqwWWrmYosMyw2AT3W0sx8jl2mCeimg=";
+  }
+  # Microsoft
+  {
+      name = "remote-explorer";
+      publisher = "ms-vscode";
+      version = "latest";
+      sha256 = "sha256-YExf9Yyo7Zp0Nfoap8Vvtas11W9Czslt55X9lb/Ri3s=";
+  }
+  # Git
+  {
+      name = "git-extension-pack";
+      publisher = "donjayamanne";
+      version = "latest";
+      sha256 = "sha256-g9uHWaKVkQtEEjU2TzZTOrPVCAbbO/BVSCEvXUvAlUg=";
+  }
+  {
+      name = "vscode-open-in-github";
+      publisher = "ziyasal";
+      version = "latest";
+      sha256 = "sha256-uJGCCvg6fj2He1HtKXC2XQLXYp0vTl4hQgVU9o5Uz5Q=";
+  }
+  # Misc
+  {
+      name = "vscode-copy-filename";
+      publisher = "bradzacher";
+      version = "latest";
+      sha256 = "sha256-elJ7bu3b9vJ2pSaI/9ve6jNKFMD3o6Kp/Ao/zqj/r0w=";
+  }
+  {
+      name = "auto-complete-tag";
+      publisher = "formulahendry";
+      version = "latest";
+      sha256 = "sha256-X7/LdajN98ySJs7XbnRzW/09TbJ23ZZo1J/yEzh9tZM=";
+  }
+  {
+      name = "vscode-todo-highlight";
+      publisher = "wayou";
+      version = "latest";
+      sha256 = "sha256-CQVtMdt/fZcNIbH/KybJixnLqCsz5iF1U0k+GfL65Ok=";
   }
 ]
