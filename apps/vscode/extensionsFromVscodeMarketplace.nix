085d0c5517e0d6cfe9cd6edcfafe39f3e688973b
Kellan Stevens <kellan@kellanstevens.com>
2024-10-20 19:36:09 +0200

    feat(apps/vscode/extensionsFromVscodeMarketplace.nix): seperate fiel for marketplace extenstions

---
+++ apps/vscode/extensionsFromVscodeMarketplace.nix
@@ -0,0 +1,46 @@
+{ pkgs }: [
+  # Looks
+  {
+      name = "moxer-icons";
+      publisher = "equinusocio";
+      version = "latest";
+      sha256 = "sha256-G63DUhg65Qv7LpKdZ4VFZCBtMIpFfoKjJs3b+2krEfM=";
+  }
+  {
+      name = "apc-extension";
+      publisher = "drcika";
+      version = "latest";
+      sha256 = "sha256-fq2qlxc7+MpwkraVClB0FGsR0K1/2MTzQ0SYULMx4Kg=";
+  }
+  {
+      name = "vscode-custom-css";
+      publisher = "be5invis";
+      version = "latest";
+      sha256 = "sha256-pVGBJjPrpjBapR9TyILgbAfpHSHXfGQMpEBwTrD3T18=";
+  }
+  # Languages
+  {
+      name = "twig-language-2";
+      publisher = "mblode";
+      version = "latest";
+      sha256 = "sha256-JbcygVMAsjF+jWZ7AlxkR+Q7skX1VYvgc0veSA1vsiU=";
+  }
+  {
+      name = "vscode-react-native";
+      publisher = "msjsdiag";
+      version = "latest";
+      sha256 = "sha256-zryzoO9sb1+Kszwup5EhnN/YDmAPz7TOQW9I/K28Fmg=";
+  }
+  {
+      name = "vscode-css-peek";
+      publisher = "pranaygp";
+      version = "latest";
+      sha256 = "sha256-JbcygVMAsjF+jWZ7AlxkR+Q7skX1VYvgc0veSA1vsiU=";
+  }
+  {
+      name = "es7-react-js-snippets";
+      publisher = "dsznajder";
+      version = "latest";
+      sha256 = "sha256-JbcygVMAsjF+jWZ7AlxkR+Q7skX1VYvgc0veSA1vsiU=";
+  }
+]
