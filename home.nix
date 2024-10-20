33c04e33fda0afe2a2863d4fa25945b1926430cc
Kellan Stevens <kellan@kellanstevens.com>
2024-10-20 13:44:01 +0200

    refactor(home.nix): moved vscode import to inline

--- home.nix
+++ home.nix
@@ -2,13 +2,10 @@
 
 { config, pkgs, ... }:
 
-let
-    vscodeConfig = import ./apps/vscode.nix { inherit config pkgs; };
-in
 {
     home.stateVersion = "24.11";
 
-    programs.vscode = vscodeConfig.config.programs.vscode;
+    programs.vscode = (import ./apps/vscode.nix { inherit config pkgs; }).config.programs.vscode;
 
     programs.home-manager.enable = true;
 
