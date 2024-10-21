5b5a430217e1523e0de9688ff6b16cb2de39687e
Kellan Stevens <kellan@kellanstevens.com>
2024-10-21 21:11:31 +0200

    chore(apps/vscode/keybindings.nix): added new keybindings

--- apps/vscode/keybindings.nix
+++ apps/vscode/keybindings.nix
@@ -4,4 +4,14 @@
     command = "editor.action.changeAll";
     when = "editorTextFocus && !editorReadonly";
   }
+  {
+    key = "";
+    command = "workbench.action.terminal.openNativeConsole";
+    when = "!terminalFocus";
+  }
+  {
+    key = "shift+cmd+down";
+    command = "cursorBottomSelect";
+    when = "textInputFocus";
+  }
 ]
