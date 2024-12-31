8c20097b492cf251ad78cec17782013e6409ec77
Kellan Stevens <kellan@kellanstevens.com>
2024-12-31 13:04:01 +0200

    chore(home.nix): new config file links

--- home.nix
+++ home.nix
@@ -5,17 +5,19 @@
 {
     home.stateVersion = "24.11";
 
-    programs.vscode = (import ./apps/vscode/configuration.nix { inherit config pkgs; }).config.programs.vscode;
-
     programs.home-manager.enable = true;
 
     home.sessionVariables = {
         EDITOR = "nvim";
     };
 
-    home.file.".zshrc".source = ./.dotfiles/zsh/.zshrc;
+    home.file.".zshrc".source = ./.config/zsh/.zshrc;
     home.file.".zsh/aliases.zsh".source = ./.config/zsh/aliases.zsh;
     home.file.".zsh/keybindings.zsh".source = ./.config/zsh/keybindings.zsh;
 
     home.file.".config/nvim/init.lua".source = ./.config/nvim/init.lua;
+
+    home.file.".config/ohmyposh/config.toml".source = ./.config/ohmyposh/config.toml;
+
+    home.file.".config/vscode/settings.json".source = ./.config/vscode/settings.json;
 }
