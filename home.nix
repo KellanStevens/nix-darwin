a71bbd82801744982b4ee52732125051db08000d
Kellan Stevens <kellan@kellanstevens.com>
2025-01-12 21:27:30 +0200

    chore(home.nix): cleanup

--- home.nix
+++ home.nix
@@ -3,22 +3,21 @@
 { config, pkgs, ... }:
 
 {
-    home.stateVersion = "24.11";
-
     programs.home-manager.enable = true;
+    home = {
+        stateVersion = "24.11";
+        sessionVariables = {
+            EDITOR = "nvim";
+        };
 
-    home.sessionVariables = {
-        EDITOR = "nvim";
+        file = {
+            ".zshrc".source = ./.config/zsh/.zshrc;
+            ".zsh/aliases.zsh".source = ./.config/zsh/aliases.zsh;
+            ".zsh/keybindings.zsh".source = ./.config/zsh/keybindings.zsh;
+            ".config/nvim/init.lua".source = ./.config/nvim/init.lua;
+            ".config/vscode/style.css".source = ./.config/vscode/style.css;
+            ".config/ohmyposh/config.toml".source = ./.config/ohmyposh/config.toml;
+            "Library/Application Support/Code/User/settings.json".source = ./.config/vscode/settings.json;
+        };
     };
-
-    home.file.".zshrc".source = ./.config/zsh/.zshrc;
-    home.file.".zsh/aliases.zsh".source = ./.config/zsh/aliases.zsh;
-    home.file.".zsh/keybindings.zsh".source = ./.config/zsh/keybindings.zsh;
-
-    home.file.".config/nvim/init.lua".source = ./.config/nvim/init.lua;
-
-    home.file.".config/ohmyposh/config.toml".source = ./.config/ohmyposh/config.toml;
-
-    home.file.".config/vscode/style.css".source = ./.config/vscode/style.css;
-    home.file."Library/Application Support/Code/User/settings.json".source = ./.config/vscode/settings.json;
 }
