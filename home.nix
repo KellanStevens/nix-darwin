f8ae3941857946bbaecbb0b421289996964a056d
Kellan Stevens <kellan@kellanstevens.com>
2025-01-17 19:47:43 +0200

    chore(home.nix): removed homemanager links

--- home.nix
+++ home.nix
@@ -9,15 +9,5 @@
         sessionVariables = {
             EDITOR = "nvim";
         };
-
-        file = {
-            ".zshrc".source = ./.config/zsh/.zshrc;
-            ".zsh/aliases.zsh".source = ./.config/zsh/aliases.zsh;
-            ".zsh/keybindings.zsh".source = ./.config/zsh/keybindings.zsh;
-            ".config/nvim/init.lua".source = ./.config/nvim/init.lua;
-            ".config/vscode/style.css".source = ./.config/vscode/style.css;
-            ".config/ohmyposh/config.toml".source = ./.config/ohmyposh/config.toml;
-            "Library/Application Support/Code/User/settings.json".source = ./.config/vscode/settings.json;
-        };
     };
 }
