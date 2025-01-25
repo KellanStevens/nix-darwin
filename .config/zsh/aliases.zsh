9ba11b9ad98f78d68f078a9829c53319d7aa17b2
Kellan Stevens <kellan@kellanstevens.com>
2025-01-25 16:01:55 +0200

    chore(.config/zsh/aliases.zsh): new alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -19,3 +19,5 @@ alias vim='nvim'
 alias android_emu='emulator -avd Pixel_6_Pro_API_35_rootable_ -writable-system -no-snapshot-load'
 
 alias rebuild-nix='darwin-rebuild switch --flake ~/.config/nix'
+
+alias tl='sudo -v && tl'
