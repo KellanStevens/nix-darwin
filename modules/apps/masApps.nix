3d7afb9d2d8b6725f25db4fcd2bdd59eb4649e54
Kellan Stevens <kellan@kellanstevens.com>
2025-03-15 10:21:32 +0200

    chore(modules/apps/masApps.nix): re-ordered alphabetically

--- modules/apps/masApps.nix
+++ modules/apps/masApps.nix
@@ -1,8 +1,8 @@
 {
-    "Xcode" = 497799835;
+    "Apple Configurator" = 1037126344;
     "Final Cut Pro" = 424389933;
     "Mactracker" = 430255202;
-    "Apple Configurator" = 1037126344;
     "Speedtest by Ookla" = 1153157709;
     "TestFlight" = 899247664;
+    "Xcode" = 497799835;
 }
